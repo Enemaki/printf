fd0845a6ee29ce13cfd310afcc3fc031dd16c038
Junioromondi <omondi.junior@yahoo.com>
2022-08-08 06:03:00 -0700

    putchar

--- _putchar.c
+++ _putchar.c
@@ -7,5 +7,8 @@
  * Return: On success 1.
  * On error, -1 ir returned, and error is set appropriately
  * 
- * /
- int _putchar(char c)
+ */
+ int _putchar(char c);
+{
+ return (write(1, &c, 1)); 
+}
