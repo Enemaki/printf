e11eb30a3aeb9630a38ccd8cedb06923e13e5cb3
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-09 11:27:58 -0700

    task 0 corr3

--- _printf.c
+++ _printf.c
@@ -6,42 +6,41 @@
  */
 int _printf(const char *format, ...)
 {
-	int i;
+	int i = 0;
 	int printed;
 	va_list args;
 
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+	while (format[i])
 	{
 		while (format[i] == '%')
 		{
 			switch (format[i + 1])
 			{
 				case 'c':
-					printed = print_char(args);
+					printed += print_char(args);
 					i = i + 2;
 					break;
 				case 's':
-					printed = print_string(args);
+					printed += print_string(args);
 					i = i + 2;
 					break;
 				case '%':
 					_putchar('%');
-					printed = 1;
+					printed += 1;
 					i = i + 2;
 					break;
 				default:
 					_putchar(format[i]);
 					_putchar(format[i + 1]);
+					printed += 2;
 					i = i + 2;
 					break;
 			}
 		}
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			printed++;
-		}
+		_putchar(format[i]);
+		printed++;
+		i++;
 	}
 	va_end(args);
 
