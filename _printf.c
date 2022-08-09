55c374caf7605cda99537c1bf36896b83d594d36
Sadiku Mustapha <mustyeneye@gmail.com>
2022-08-09 08:08:26 -0700

    task 0 corr 2

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		while (format[i] == '%')
 		{
 			switch (format[i + 1])
 			{
